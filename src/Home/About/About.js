4b7ae0aa203db43054e2a3679679aa04d733a4af
tanvir <himonislamfre@gmail.com>
2022-04-18 20:57:55 +0600

    commit

--- src/Home/About/About.js
+++ src/Home/About/About.js
@@ -6,6 +6,8 @@ const About = () => {
         <div className='about-image'>
             <h2>About Me</h2>
             <img src={myImage} alt="" />
+            <h2>Number:01728XXXXXX</h2>
+            <h2>Email: <small>himonxxxxxxx@gmail.com</small></h2>
         </div>
     );
 };
